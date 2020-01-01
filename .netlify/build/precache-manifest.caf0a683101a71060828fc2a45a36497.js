142e2a7fa7b4484623c326fa1eab4594247ccba7
mmur-cse <maryam77338@gmail.com>
2020-01-01 20:28:18 +0530

    first commit

---
+++ .netlify/build/precache-manifest.caf0a683101a71060828fc2a45a36497.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e07749ac03a9276e056ee8dd27fc8b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3ce5a3002bb53b257b5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "96999fc5cdf81a8a7c0a",
+    "url": "/static/js/2.578366c1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.578366c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3ce5a3002bb53b257b5",
+    "url": "/static/js/main.6a81ec4e.chunk.js"
+  },
+  {
+    "revision": "a1266c636d93f2172679",
+    "url": "/static/js/runtime-main.2afc97ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
